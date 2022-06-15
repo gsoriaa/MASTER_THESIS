4843d612f2b3bbaece426618e77ffdc5af40d498
gsoriaa <76224323+gsoriaa@users.noreply.github.com>
2022-06-15 11:57:22 +0200

    STEP 3: Scripts to extract DEGs and run enrichment analysis
    Upload scripts generated to extract DEGs and perform enrichment analysis on the .Rds object generated in STEP-2

---
+++ 3.1-DEGs_extract.R
@@ -0,0 +1,222 @@
+library(Seurat)
+library(dplyr)
+library(ggrepel)
+library(SeuratObject)
+##
+### Leemos el objeto creado como resultado del filtrado y normalización de mis datos en Bollito
+dir.create("./degs_analysis")
+assay_seurat <- readRDS("./out_AMs/seurat/merged/3_clustering/seurat_find-clusters.rds")
+
+#
+## Para llevar a cabo el análisis de expresión diferencial (DEA) 
+## primero debemos ver qué tipo de datos tenemos. Hay que comprobar
+## qué distribución siguen mis datos para escoger el test adecuado.
+#
+
+##SHAPIRO-WILK TEST PARA VER LA DISTRIBUCIÓN:
+shapiro.test(assay_seurat@meta.data$nFeature_SCT) # W = 0.95477, p-value < 2.2e-16
+shapiro.test(assay_seurat@meta.data$nCount_SCT) # W = 0.93633, p-value < 2.2e-16
+#p<0.05 por tanto es distribucion no normal
+
+###Al no seguir una distribución normal, puedo aplicar el wilcoxon rank sum test como DEA
+#*si p>0.05, la H0 de que mis datos siguen una distribución normal 
+#no se puede descartar y por tanto tendría que emplear otro
+#tipo de test estadístico (como un t-test) como método de DEA
+
+## DEFINIR EL assay_seurat DE SCT
+DefaultAssay(assay_seurat) <- "SCT"
+# DEFINIR LA COLUMNA DE METADATA QUE SE EMPLEARÁ POR DEFECTO PARA DIFERENCIAR MIS CÉLULAS
+Idents(assay_seurat) = assay_seurat@meta.data$group
+
+## IDENTIFICAR LOS GENES MARCADORES QUE DEFINEN CADA UNO DE LOS CLUSTERS DE PICs
+## FIGURA 6
+MARKER_PICs <- FindAllMarkers(subset(assay_seurat, group %in% c("EMT_PICs", "PROLIF_PICs", "STRESS_PIC")),
+                              test.use = "wilcox", group.by = "group")
+dim(MARKER_PICs) #462
+MARKER_PICs = MARKER_PICs %>% filter(p_val_adj < 0.05) %>% filter(abs(avg_log2FC) > 0.45)
+length(unique(MARKER_PICs$gene)) # 62 genes únicos
+# Están los tres genes definitorios de cada cluster en mi set?
+# 
+c("Ccnd1", "Clu", "Hmga2") %in% MARKER_PICs$gene # TRUE TRUE TRUE
+
+# Ordenamos los genes para representarlos de forma más visual en un heatmap
+# 
+stress_genes <- c()
+prolif_genes <- c()
+emt_genes <- c()
+for (gene_id in unique(MARKER_PICs$gene)){
+top_C = MARKER_PICs %>% filter(gene == gene_id) %>% arrange(desc(avg_log2FC)) %>% top_n(1, avg_log2FC)
+#ordenar segun la expresion del gen y coger el top 1
+if (top_C$cluster == "STRESS_PIC"){ #asignarlo al cluster que me interese
+stress_genes <- c(stress_genes, top_C$gene)
+}
+if (top_C$cluster == "PROLIF_PICs"){ #asignarlo al cluster que me interese
+prolif_genes <- c(prolif_genes, top_C$gene)
+}
+if (top_C$cluster == "EMT_PICs"){ #asignarlo al cluster que me interese
+emt_genes <- c(emt_genes, top_C$gene)
+}
+}
+marker_features <- c(emt_genes, prolif_genes, 
+                     rev(stress_genes)) #rev() para cambiar el cluster de estrés y 
+                                        # agrupar Ccnd1 con su cluster de expresión (proliferación)
+
+pdf("FIGURA_6-PICs_marker_genes.pdf") 
+DoHeatmap(subset(assay_seurat, group %in% c("EMT_PICs", "PROLIF_PICs", "STRESS_PIC")), 
+          marker_features, group.by= "group")
+dev.off()
+
+#######################################################
+########## IDENTIFICAR EMT_PIC DEGs ###################
+#######################################################
+####
+####Aquí se procede a realizar el flujo esquematizado en la FIGURA 16
+####
+### PASO 1: DEFINIR LA PRIMERA APROXIMACIÓN DE DEGs
+### MIN_LFC = 0.25
+## A) TUMOR DEGs: EMT_S x EMT_P
+DEGs_EMTPICs_EMT<- FindMarkers(assay_seurat, ident.1 = "EMT_PICs", ident.2 = "EMT_singlet", group.by = "group")
+dim(DEGs_EMTPICs_EMT) #196 DEGs (sin filtrar)
+Type<-c()
+for (gene in rownames(DEGs_EMTPICs_EMT)) {
+  if (DEGs_EMTPICs_EMT[gene, "avg_log2FC"] > 0){Type<-c(Type,"Up-regulated")} 
+  else {Type<-c(Type,"Down-regulated")}
+}
+DEGs_EMTPICs_EMT$Type<-factor(Type, levels = c("Up-regulated", "Down-regulated"))
+write.table(DEGs_EMTPICs_EMT, "DEGs_EMTPICs_EMT_nofilter.tsv", quote=FALSE, row.names = TRUE, sep = "\t")
+DEGs_EMTPICs_EMT<-dplyr::filter(DEGs_EMTPICs_EMT,
+                                 p_val_adj < 0.05 )
+
+write.table(DEGs_EMTPICs_EMT, "DEGs_EMTPICs_EMT.tsv", quote=FALSE, row.names = TRUE, sep = "\t")
+dim(DEGs_EMTPICs_EMT) #76 DEGs significativos asociados a tumor (log2FC > 0.25)
+
+##B) AM DEGs: AMs x EMT_P
+DEGs_PICs_AM<-FindMarkers(assay_seurat, ident.1 = "EMT_PICs", ident.2 = "AMs", group.by = "group")
+dim(DEGs_PICs_AM) #847
+Type<-c()
+for (gene in rownames(DEGs_PICs_AM)) {
+  if(DEGs_PICs_AM[gene, "avg_log2FC"] > 0){Type<-c(Type,"Up-regulated")} 
+  else {Type<-c(Type,"Down-regulated")}
+}
+DEGs_PICs_AM$Type<-factor(Type, levels = c("Up-regulated", "Down-regulated"))
+write.table(DEGs_PICs_AM, "DEGs_PICs_AM_nofilter.tsv", quote=FALSE, row.names = TRUE, sep = "\t")
+DEGs_PICs_AM<-dplyr::filter(DEGs_PICs_AM,
+                                 p_val_adj < 0.05 )
+write.table(DEGs_PICs_AM, "DEGs_PICs_AM.tsv", quote=FALSE, row.names = TRUE, sep = "\t")
+dim(DEGs_PICs_AM) #749 DEGs significativos asociados a AM (log2FC > 0.25)
+
+### STEP 2: FILTRAR AMBOS SETS DE GENES
+## DEFINIMOS GENES BASALES QUE SE EXPRESAN MÁS EN UNO U
+## OTRO TIPO DE CÉLULAS:  singletes de AM vs. singletes de tumor
+DEGs_singlets <- FindMarkers(assay_seurat, ident.1 = c("EMT_singlet","PROLIF_singlet","STRESS_singlet"), ident.2 = "AMs", group.by = "group")
+DEGs_singlets<-dplyr::filter(DEGs_singlets,
+                                p_val_adj < 0.05 )
+dim(DEGs_singlets) #[1] 829 5
+DEGs_singlets_AM<-dplyr::filter(DEGs_singlets,
+                                 avg_log2FC<0)
+dim(DEGs_singlets_AM) #194
+#
+DEGs_singlets_TUM<-dplyr::filter(DEGs_singlets,
+                                 avg_log2FC>0)
+dim(DEGs_singlets_TUM) #635
+
+#####PROBLEMA: los genes pueden aparecer como consecuencia del ruido de fondo
+##### aportado por la segunda célula en interacción (y que no es de interés)
+
+## Para empezar, hay 2 opciones: 
+## 1- genes COMPARTIDOS entre ambos sets
+## 2- genes ÚNICOS para uno de los sets
+
+#1- Si los genes son COMUNES, no aparecen como consecuencia del ruido de fondo (al menos en uno de los casos)
+## 4 OPCIONES:
+## A.Si el gen se expresa más en ambos cluster de singlete que en el PIC:
+#	| T_sing		   AM_sing
+#	| 	      PIC			          => No podemos saber el origen del DEG, podría verse afectado 
+# |______________________
+## B.Si el gen se expresa menos en ambos cluster de singlete que en el PIC:
+#|          PIC
+#|	T_sing	  	 AM_sing			=> No podemos saber el origen del DEG, podría verse afectado 
+#|_______________________
+## C.Si el gen se expresa más en tumor pero menos en AM
+#|	T_sing	  
+#|		      PIC	
+#|			         AM_sing			=> El gen se puede asociar a tumor
+#|_______________________
+## D.Si el gen se expresa menos en tumor pero más en AM
+#|		  	       AM_sing
+#|		      PIC	
+#|	T_sing					          => El gen se puede asociar a AM
+#|_______________________
+
+############## Número de genes en cada opción:
+up_DEGs_EMTPICs_EMT = DEGs_EMTPICs_EMT %>% filter(Type == 'Up-regulated') %>% rownames() # 71
+down_DEGs_EMTPICs_EMT = DEGs_EMTPICs_EMT %>% filter(Type == 'Down-regulated') %>% rownames() #5 
+up_DEGs_PICs_AM = DEGs_PICs_AM %>% filter(Type == 'Up-regulated') %>% rownames() #711
+down_DEGs_PICs_AM = DEGs_PICs_AM %>% filter(Type == 'Down-regulated') %>% rownames() #38
+
+sum(up_DEGs_EMTPICs_EMT %in% up_DEGs_PICs_AM) #0
+sum(down_DEGs_EMTPICs_EMT %in% down_DEGs_PICs_AM) #0
+sum(up_DEGs_EMTPICs_EMT %in% down_DEGs_PICs_AM) #26, 100% tumor genes
+sum(down_DEGs_EMTPICs_EMT %in% up_DEGs_PICs_AM) #4, 100% AM genes
+
+
+####SI LOS GENES NO SON COMUNES
+# Se comprueba la expresión en singletes para filtrar estos genes 
+# de acuerdo al tipo celular al que mayormente se puede asociar 
+# su expresión  
+
+#### EN RESUMEN, simplemente estamos eliminando genes
+#### no comunes SOBREEXPRESADOS en los PICs que son:
+#1- A-DEGs %in% AM_singlet genes (BG-noise-affected)
+#2- B-DEGs %in% TUM_singlet genes (BG-noise-affected)
+
+#1-
+#Definimos el set de genes únicos asociados a AM sobre-expresados
+# en el PIC:
+#UP-GENES (71 genes)
+#unique up-genes (45 genes)
+unique_up_TUM = up_DEGs_EMTPICs_EMT[up_DEGs_EMTPICs_EMT %in% 
+                                      rownames(DEGs_PICs_AM) == 
+                                      FALSE]
+
+#Definimos el set de genes eliminados que se asocian a AM erróneamente
+trimmed_genes_TUM = unique_up_TUM[unique_up_TUM %in% 
+                                    rownames(DEGs_singlets_AM)]
+
+#filtramos los DEGs:
+TUM_DEGs = rownames(DEGs_EMTPICs_EMT[
+  rownames(DEGs_EMTPICs_EMT) %in% trimmed_genes_TUM == FALSE,])
+length(TUM_DEGs) #33 genes
+
+#2-
+#Definimos el set de genes únicos asociados a tumor y sobre-expresados en el PIC:
+#UP-GENES (711 genes)
+#unique up-genes (707 genes)
+unique_up_AM = up_DEGs_PICs_AM[up_DEGs_PICs_AM %in% rownames(DEGs_EMTPICs_EMT) == FALSE]
+
+#Definimos el set de genes eliminados que se asocian erróneamente a tumor
+trimmed_genes_AM = unique_up_AM[unique_up_AM %in% rownames(DEGs_singlets_TUM)]
+
+#filtramos los DEGs:
+AM_DEGs = rownames(DEGs_PICs_AM[rownames(DEGs_PICs_AM) %in% trimmed_genes_AM == FALSE,])
+length(AM_DEGs) #388 genes
+
+sum(TUM_DEGs %in% AM_DEGs) # 30 de los genes son comunes 
+TUM_DEGs = TUM_DEGs[TUM_DEGs %in% up_DEGs_PICs_AM ==FALSE] #elimino los 4 genes que se asocian a AM
+AM_DEGs = AM_DEGs[AM_DEGs %in% up_DEGs_EMTPICs_EMT ==FALSE] #elimino los 26 genes comunes que se asocian a tumor
+
+length(TUM_DEGs) # 29
+length(AM_DEGs) # 362
+
+DEGs_EMTPICs_EMT_res = DEGs_EMTPICs_EMT[TUM_DEGs,] #DF
+write.table(DEGs_EMTPICs_EMT_res, "DEGs_EMTPICs_EMT_res.tsv", quote=FALSE, row.names = TRUE, sep = "\t")
+DEGs_PICs_AM_res = DEGs_PICs_AM[AM_DEGs,] #DF
+write.table(DEGs_PICs_AM_res, "DEGs_PICs_AM_res.tsv", quote=FALSE, row.names = TRUE, sep = "\t")
+
+## Combino los genes
+combined_DEGs_names = unique(c(TUM_DEGs,AM_DEGs))
+combined_DEGs <- rbind(DEGs_EMTPICs_EMT_res, DEGs_PICs_AM_res)
+
+write.table(combined_DEGs_names, "combined_DEGs_names_res.tsv", quote=FALSE, row.names = F, sep = "\t")
+write.table(combined_DEGs, "combined_res.tsv", quote=FALSE, row.names = T, sep = "\t")
+##
---
+++ 3.2-Figuras_&_Enrichment.R
@@ -0,0 +1,202 @@
+library("ggrepel")
+library("stringr")
+library("ggvenn")
+library(readxl)
+library(ggplot2)
+#
+library(clusterProfiler); packageDescription ("clusterProfiler", fields = "Version") 
+library(topGO)
+#library(org.Hs.eg.db) #homo sapiens
+library(org.Mm.eg.db) #mus musculus
+library(stringr)
+
+###########################1-
+############################# CARGAR LOS DEGs de EMT_PICs vs. EMT_singlets
+
+DEGs_EMTPICs_EMT<-read.table(file = "./DEGs_EMTPICs_EMT.tsv", sep = "\t", header = TRUE)
+dim(DEGs_EMTPICs_EMT) #76
+
+DEGs_EMTPICs_EMT_final<-read.table(file = "./DEGs_EMTPICs_EMT_res.tsv", sep = "\t", header = TRUE)
+dim(DEGs_EMTPICs_EMT_final) #29
+
+## ELIMINAMOS LOS GENES MITOCONDRIALES
+DEGs_EMTPICs_EMT_final_noMith<-DEGs_EMTPICs_EMT_final[
+  str_detect(rownames(DEGs_EMTPICs_EMT_final), 
+             regex("^mt-", ignore_case = TRUE)) == FALSE,]
+
+dim(DEGs_EMTPICs_EMT_final_noMith) #27
+
+write.table(rownames(DEGs_EMTPICs_EMT_final_noMith), "genes_TUM_nomith.txt",
+            quote=FALSE, row.names = FALSE)
+write.table(DEGs_EMTPICs_EMT_final_noMith, "DEGs_EMTPICs_EMT_final_noMith.tsv", 
+            quote=FALSE, row.names = TRUE, sep = "\t")
+
+###VOLCANOPLOT DE ESTE DATASET (FIGURA 8A)
+DEGs_EMTPICs_EMT$Type<-factor(DEGs_EMTPICs_EMT$Type, 
+                              levels = c("Up-regulated", "Down-regulated"))
+pdf("./FIGURA_8A-volcanoplot_DEGs_EMT.pdf")
+p<- ggplot(data=DEGs_EMTPICs_EMT, 
+           aes(x=avg_log2FC, y=-log10(p_val_adj), col=Type)) + 
+  geom_point() + theme_minimal()
+# Añadimos las líneas
+p2 <- p + geom_vline(xintercept = c(-0.25,0.25),col="red", linetype = "dashed") + 
+  geom_vline(xintercept = 0,col="red") + 
+  geom_hline(yintercept=-log10(0.05), col="red") + 
+  ggtitle("VolcanoPlot DEGs EMT_singlets vs EMT_PICs")
+# Destacamos los genes filtrados finales
+p3 <- p2 +   geom_point(data=DEGs_EMTPICs_EMT[rownames(DEGs_EMTPICs_EMT_final_noMith),], 
+                        aes(x=avg_log2FC,y=-log10(p_val_adj)), 
+                        color='gold',
+                        pch=6,
+                        size=4)
+
+p3
+#PLOT con nombres
+####
+ggplot(DEGs_EMTPICs_EMT_final_noMith,
+       aes(x=avg_log2FC, y = -log(p_val_adj)),
+       ylim())+
+  geom_point() + # Show dots
+  geom_text_repel(aes(label = rownames(DEGs_EMTPICs_EMT_final_noMith)),
+                  min.segment.length = 0,
+                  box.padding = 0.2,
+                  max.overlaps = 15
+  ) +
+  ggtitle("EMT- PIC vs. singlets DEGs")
+dev.off()
+###########################2-
+############################# CARGAMOS LOS DEGs de AMs vs. EMT_PICs
+
+DEGs_PICs_AM<-read.table(file = "DEGs_PICs_AM.tsv", sep = "\t", header = TRUE)
+dim(DEGs_PICs_AM) #749
+
+DEGs_PICs_AM_final<-read.table(file = "DEGs_PICs_AM_res.tsv", sep = "\t", header = TRUE)
+dim(DEGs_PICs_AM_final) #362
+
+## ELIMINAMOS GENES MITOCONDRIALES
+DEGs_PICs_AM_final_noMith<-DEGs_PICs_AM_final[
+  str_detect(rownames(DEGs_PICs_AM_final),
+             regex("^mt-", ignore_case = TRUE)) == FALSE,]
+
+dim(DEGs_PICs_AM_final_noMith) #360
+
+write.table(rownames(DEGs_PICs_AM_final_noMith), "genes_AM_nomith.txt",
+            quote=FALSE, row.names = FALSE)
+write.table(DEGs_PICs_AM_final_noMith, "DEGs_PICs_AM_final_noMith.tsv", 
+            quote=FALSE, row.names = TRUE, sep = "\t")
+
+###VOLCANOPLOT DE ESTE DATASET (FIGURA 8B)
+
+pdf("./FIGURA_8B-volcanoplot_DEGs_AM.pdf")
+DEGs_PICs_AM$Type<-factor(DEGs_PICs_AM$Type, levels = c("Up-regulated", "Down-regulated"))
+p<- ggplot(data=DEGs_PICs_AM, aes(x=avg_log2FC, y=-log10(p_val_adj), col=Type)) +
+  geom_point() + theme_minimal()
+
+p2 <- p + geom_vline(xintercept = c(-0.25,0.25),col="red", linetype = "dashed") +
+  geom_vline(xintercept = 0,col="red") + geom_hline(yintercept=-log10(0.05), col="red") +
+  ggtitle("VolcanoPlot DEGs AM_singlets vs. EMT_PICs")
+
+p3 <- p2 +   geom_point(data=DEGs_PICs_AM_final_noMith, 
+                        aes(x=avg_log2FC,y=-log10(p_val_adj)), 
+                        color='gold',
+                        pch=6,
+                        size=3)
+
+p3
+
+ggplot(DEGs_PICs_AM_final_noMith,
+       aes(x=avg_log2FC, y = -log(p_val_adj)),
+       ylim())+
+  geom_point() + # Show dots
+  geom_text_repel(aes(label = rownames(DEGs_PICs_AM_final_noMith)),
+                  min.segment.length = 0,
+                  box.padding = 0.2,
+                  max.overlaps=20
+  ) +
+  ggtitle("AM_singlets VS. EMT_PIC DEGs")
+
+dev.off()
+
+###############################
+###############################
+
+###ANÁLISIS DE RUTAS ENRIQUECIDAS EN MI SET DE GENES DIFERENCIALMENTE EXPRESADOS: 
+############### 
+############### CLUSTERPROFILER
+############### 
+
+genes_full_combined = c(rownames(DEGs_EMTPICs_EMT_final_noMith), 
+                        rownames(DEGs_PICs_AM_final_noMith))
+genes_full_combined #387 genes
+
+ego_full <- enrichGO(gene          = genes_full_combined, #genes
+                     OrgDb         = org.Mm.eg.db, #organismo de referencia (ratón, mus musculus)
+                     keyType       = 'SYMBOL', #código de los genes
+                     ont           = "BP", # Ontología de referencia: Proceso biológico
+                     pAdjustMethod = "BH", # Corrección por Benjamini - Hochberg
+                     pvalueCutoff  = 0.05, # límite de p-valor 0.05
+                     qvalueCutoff  = 0.05) # límite de q-value 0.05
+#* En ont: BP, MF (Molecular function), CC (Cellular component) o ALL (para emplear todas las 
+#* ontologías como referencia).
+
+# Cuántos procesos se asocian significativamente
+dim(ego_full) #636 if q-val = 0.05
+
+# FIGURA 8C
+pdf("FIGURA_8C-enrichment_dotplot_TOP25.pdf")
+dotplot(ego_full, showCategory=nrow(ego_full[0:25])) 
+dev.off()
+##### Visualización de rutas relevante para el proceso de EMT
+key_terms<- c("epith","epiderm","mesench","adhesion",
+              "migration","angiogen","growth","hypoxia",
+              "beta","p53","nfkb", "epidermal",
+              "endothelial","extracel", "matrix", "kinase B",
+              "tgf", "move", "chemotax", "remodel","grow","blood") 
+
+# Me quedo con las categorías que contengan palabras clave
+enriched_cathegories<-c()
+for (term in key_terms){
+  for (cathegory in ego_full@result$Description[ego_full@result$p.adjust < 0.05]){
+    if (is.na(cathegory) == FALSE){
+      if (str_detect(cathegory, term) == TRUE){
+        enriched_cathegories<-c(enriched_cathegories, cathegory)
+      }
+    }
+  }
+}
+ego_full_key<-dplyr::filter(ego_full,
+                            ego_full@result$Description %in% enriched_cathegories)
+
+# 72
+pdf("FIGURA_8D-enrichment_dotplot_key.pdf")
+dotplot(ego_full_key, showCategory=nrow(ego_full_key[0:30]))
+dev.off()
+
+################
+################ FIRMAS DE EMP (Cook & Vanderhyden, 2022)
+################
+
+EMP_conserved_signature <- as.vector(as.matrix(
+  read.table("EMP_conserved_signature.tsv", header = F)))
+malignant_conserved_signature <- as.vector(as.matrix(
+  read.table("Malignant_cell_signature.tsv", header = F)))
+
+length(EMP_conserved_signature) #328 genes en la firma general conservada de EMP
+length(malignant_conserved_signature) #128 genes en la firma conservada específica de célula tumoral
+
+sum(EMP_conserved_signature %in% genes_full_combined)/328*100 #17 genes, 5.18%
+sum(malignant_conserved_signature %in% genes_full_combined)/128*100 #5 genes, 3.9%
+#  
+pdf("FIGURA_9-DCOOK_EMP_SIGNATURES-venn.pdf") #FIGURA 9
+x <- list(
+  "EMP_conserved_signature" = EMP_conserved_signature, 
+  "DEGs_of_interest" = genes_full_combined)
+ggvenn(x,fill_color=c("cyan","burlywood2"), show_percentage = FALSE, text_size = 5)
+
+y <- list(
+  "EMP_malignant" = malignant_conserved_signature, 
+  "DEGs_of_interest" = genes_full_combined)
+ggvenn(y,fill_color=c("cyan","burlywood2"), show_percentage = FALSE, text_size = 5)
+dev.off()
+
+# NEXT: INTERACTION ANALYSIS
