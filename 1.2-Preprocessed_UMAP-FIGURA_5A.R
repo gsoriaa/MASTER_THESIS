755d9226b41ce025e6768d116da4b875ba94352d
gsoriaa <76224323+gsoriaa@users.noreply.github.com>
2022-06-16 14:42:05 +0200

    Update 1.2-Preprocessed_UMAP-FIGURA_5A.R

--- 1.2-Preprocessed_UMAP-FIGURA_5A.R
+++ 1.2-Preprocessed_UMAP-FIGURA_5A.R
@@ -40,3 +40,30 @@ pdf("preprocessed_assay_UMAP.pdf")
 DimPlot(preprocessed_assay, label = TRUE, group.by= 'group', reduction='umap') + ggplot2::theme(legend.position = "none") #sin leyenda pero poniendo el nombre en el custer
 DimPlot(preprocessed_assay, label = FALSE, group.by= 'group', reduction='umap') # con leyenda
 dev.off()
+
+## Para ver la mediana de Counts y features empleadas en bollito para cada uno de los 3 conjuntos de datos,
+## y especificar los parámetros de los config.yaml mostrados en la tabla 1, podríamos emplear el siguiente código:
+### Por ejemplo: en el caso de los AMs
+                   
+#a. N_features (genes)
+median(as.vector(as.matrix(
+  preprocessed_assay@meta.data %>% 
+    filter(Batch %in% 
+             c('AB10173_no_Tcells','AB10177_no_Tcells', 'AB10178_no_Tcells')) %>% 
+    select(nFeature_RNA))))
+#287 es la mediana
+287/2.5 #114.8 como limite inferior
+287*2.5 #717.5 como límite superior
+
+#b. N_counts (moléculas)
+median(as.vector(as.matrix(
+  preprocessed_assay@meta.data %>% 
+    filter(Batch %in% 
+             c('AB10173_no_Tcells','AB10177_no_Tcells', 'AB10178_no_Tcells')) %>% 
+    select(nCount_RNA))))
+
+#591
+591/3 #197 como limite inferior
+591*3 #1773 como límite superior
+
+# y esto mismo aplicado a los tres tipos de fichero editando los nombres del fichero de origen.
